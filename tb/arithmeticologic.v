fc504b0ee0707bcf97078bd2950706beaf86148c
Keendraa <107687468+Keendraa@users.noreply.github.com>
2022-06-27 18:24:50 +0200

    Add files via upload

--- tb/arithmeticologic.v
+++ tb/arithmeticologic.v
@@ -11,8 +11,8 @@ module tb_arithmeticologic();
 	initial begin 
 
 		TB.pc = 32'b0;
-    //TB.top_CoreMem_inst.instr_mem.rstinstrMem;
-    //TB.iniinstrMem;
+    TB.top_CoreMem_inst.mem_instr_inst.initializeinstrMem;
+    TB.iniinstrMem;
 		// Initialize registers
 		TB.clk = 1'b0;
 		TB.rst_n = 1'b0;
@@ -20,75 +20,17 @@ module tb_arithmeticologic();
 		
 		// Load memory
 		//$readmemb("data/instrramMem_b.mem", TB.top_CoreMem_inst.mem_instr_inst.mem, 0, 3);
-		//$readmemh("../data/dataMem_h.mem", TB.top_CoreMem_inst.mem_data_inst.mem, 0, 3);
-		//$readmemh("F:/TFG/Codi_original/RV32I/data/dataMem_h.mem", TB.top_CoreMem_inst.instr_mem.sp_ram_wrap_instr_i.sp_ram_instr_i.mem_instr);
+		$readmemh("../data/dataMem_h.mem", TB.top_CoreMem_inst.mem_data_inst.mem, 0, 3);
 		
-		//$readmemh("F:/TFG/Codi_original/RV32I/data/dataMem_h.txt", TB.top_CoreMem_inst.instr_mem.sp_ram_wrap_instr_i.sp_ram_instr_i.mem_instr);
-		//$readmemh("F:/TFG/Codi_original/RV32I/data/contador.txt", TB.top_CoreMem_inst.instr_mem.sp_ram_wrap_instr_i.sp_ram_instr_i.mem_instr);
-		//TEST1------------------------------------------------------------------------
-		
-		$readmemh("F:/TFG/Codi_original/RV32I/data/dataMem_h.txt", TB.top_CoreMem_inst.instr_mem.sp_ram_wrap_instr_i.sp_ram_instr_i.mem_instr);
-		TB.test_andC;
-		TB.rst_n = 1'b0;
-		#100
-/*
 		TB.test_add;
-	  	TB.rst_n = 1'b0;
+	  TB.rst_n = 1'b0;
 		#100
 		
 		TB.test_and;
 		TB.rst_n = 1'b0;
 		#100
 
-		TB.test_andC;
-		TB.rst_n = 1'b0;
-		#100
-
-		TB.test_andC;
-		TB.rst_n = 1'b0;
-		#100
-		
-		TB.test_and;
-		TB.rst_n = 1'b0;
-		*/
-		//TEST2------------------------------------------------------------------------
-		/*
-		$readmemh("F:/TFG/Codi_original/RV32I/data/dataMem_h.txt", TB.top_CoreMem_inst.instr_mem.sp_ram_wrap_instr_i.sp_ram_instr_i.mem_instr)
-		TB.test_MIXED_andC_addI;
-		TB.rst_n = 1'b0;
-		*/
-		//TEST3:BRANCH------------------------------------------------------------------------
-		/*
-		$readmemh("F:/TFG/Codi_original/RV32I/data/dataMem_h.txt", TB.top_CoreMem_inst.instr_mem.sp_ram_wrap_instr_i.sp_ram_instr_i.mem_instr)
-		TB.test_bneqzC;
-		TB.rst_n = 1'b0;
-		#100
-
-		TB.test_beqzC;
-	  	TB.rst_n = 1'b0;
-		#100
-		*/
-		//TEST4: INSTRUCCIONS COMPRIMIDES------------------------------------------------------------------------
-		/*
-		$readmemh("F:/TFG/Codi_original/RV32I/data/dataMem_h.txt", TB.top_CoreMem_inst.instr_mem.sp_ram_wrap_instr_i.sp_ram_instr_i.mem_instr)
-		TB.test_addC;
-		TB.rst_n = 1'b0;
-		#100
-
-		TB.test_subC;
-	  	TB.rst_n = 1'b0;
-		#100
-		
-		TB.test_addiC;
-	  	TB.rst_n = 1'b0;
-		#100
-		*/
-
-		//TEST5:Restador------------------------------------------------------------------------
-		//$readmemh("F:/TFG/Codi_original/RV32I/data/restador.txt", TB.top_CoreMem_inst.instr_mem.sp_ram_wrap_instr_i.sp_ram_instr_i.mem_instr);
-
-		/*
-		TB.test_beqzC;
+		TB.test_andi;
 		#100
     TB.rst_n = 1'b0;
     #100
@@ -101,9 +43,8 @@ module tb_arithmeticologic();
     TB.rst_n = 1'b0;
     #100
 		TB.test_sltiu;
-		*/
-  #100000
+  #1000
 	$finish;
 end
 
-endmodule   
+endmodule 
---
+++ tb/compressed.v
@@ -0,0 +1,117 @@
+// Code your testbench here
+// or browse Examples
+`timescale 1ns/1ps
+
+//`include"testbench.v"
+
+module tb_arithmeticologic();
+
+	tb TB();
+	
+	initial begin 
+
+		//TEST1------------------------------------------------------------------------
+		TB.pc = 32'b0;
+		TB.clk = 1'b0;
+		TB.rst_n = 1'b0;
+		#100
+
+		$readmemh("F:/TFG/Codi_original/RV32I/data/dataMem_h.txt", TB.top_CoreMem_inst.instr_mem.sp_ram_wrap_instr_i.sp_ram_instr_i.mem_instr);
+		TB.test_andC;
+		TB.rst_n = 1'b0;
+		#100
+
+		TB.test_add;
+	  	TB.rst_n = 1'b0;
+		#100
+		
+		TB.test_and;
+		TB.rst_n = 1'b0;
+		#100
+
+		TB.test_andC;
+		TB.rst_n = 1'b0;
+		#100
+
+		TB.test_andC;
+		TB.rst_n = 1'b0;
+		#100
+		
+		TB.test_and;
+		TB.rst_n = 1'b0;
+		
+		//TEST2------------------------------------------------------------------------
+		/*
+		TB.pc = 32'b0;
+		TB.clk = 1'b0;
+		TB.rst_n = 1'b0;
+		#100
+
+		$readmemh("F:/TFG/Codi_original/RV32I/data/dataMem_h.txt", TB.top_CoreMem_inst.instr_mem.sp_ram_wrap_instr_i.sp_ram_instr_i.mem_instr)
+		TB.test_MIXED_andC_addI;
+		TB.rst_n = 1'b0;
+		*/
+		//TEST3:BRANCH------------------------------------------------------------------------
+		/*
+
+		TB.pc = 32'b0;
+		TB.clk = 1'b0;
+		TB.rst_n = 1'b0;
+		#100
+
+		$readmemh("F:/TFG/Codi_original/RV32I/data/dataMem_h.txt", TB.top_CoreMem_inst.instr_mem.sp_ram_wrap_instr_i.sp_ram_instr_i.mem_instr)
+		TB.test_bneqzC;
+		TB.rst_n = 1'b0;
+		#100
+
+		TB.test_beqzC;
+	  	TB.rst_n = 1'b0;
+		#100
+		*/
+		//TEST4: INSTRUCCIONS COMPRIMIDES------------------------------------------------------------------------
+		/*
+		TB.pc = 32'b0;
+		TB.clk = 1'b0;
+		TB.rst_n = 1'b0;
+		#100
+
+		$readmemh("F:/TFG/Codi_original/RV32I/data/dataMem_h.txt", TB.top_CoreMem_inst.instr_mem.sp_ram_wrap_instr_i.sp_ram_instr_i.mem_instr)
+		TB.test_addC;
+		TB.rst_n = 1'b0;
+		#100
+
+		TB.test_subC;
+	  	TB.rst_n = 1'b0;
+		#100
+		
+		TB.test_addiC;
+	  	TB.rst_n = 1'b0;
+		#100
+		*/
+
+		//TEST5:Restador------------------------------------------------------------------------
+		/*
+		TB.pc = 32'b0;
+
+		TB.clk = 1'b0;
+		TB.rst_n = 1'b0;
+		#100
+
+		$readmemh("F:/TFG/Codi_original/RV32I/data/restador.txt", TB.top_CoreMem_inst.instr_mem.sp_ram_wrap_instr_i.sp_ram_instr_i.mem_instr);
+  		TB.clk = 1'b0;
+  		TB.rst_n = 1'b0;
+  		#100
+		TB.rst_n = 1'b1;
+		#40000
+		
+		TB.test_load;
+		#100
+		TB.rst_n = 1'b0;
+		#100
+		*/
+
+  #100000
+	$finish;
+end
+
+endmodule   
